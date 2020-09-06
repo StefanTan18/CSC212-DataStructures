4e475673edd671102c7a03d21a0b228743a20f2e
Stefan Tan <stefantan@Stefans-Air.fios-router.home>
2020-09-06 17:04:07 -0400

    intialized stats.h and stats.cpp

---
+++ hw1/stats.cpp
@@ -0,0 +1,12 @@
+// FILE: stats.cpp
+// CLASS IMPLEMENTED: statistician  (See stats.h for documentation.)
+
+#include <iostream>
+#include <math.h>
+#include "stats.h"
+using namespace std;
+
+namespace main_savitch_2C
+{
+
+}
